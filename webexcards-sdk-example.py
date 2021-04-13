5e68fffcf71dd1521ae42aa2f765f1e707636f5e
DJ <github@nldj.com>
2021-04-13 12:53:43 +0200

    Update webexcards-sdk-example.py

--- webexcards-sdk-example.py
+++ webexcards-sdk-example.py
@@ -1,3 +1,20 @@
+# Python example for Cards with the WebexteamsSDK 
+#    DJ Uittenbogaard (duittenb@cisco.com)
+# details: https://github.com/DJF3/Webex-SDK-Cards-example/
+"""Showing how you can use Cards with the Webexteamsdk to create
+nicely formatted Webex Messages.
+Copyright (c) 2019 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
 # -*- coding: utf-8 -*-
 from webexteamssdk import WebexTeamsAPI, ApiError
 from webexteamssdk.models.cards.card import AdaptiveCard
